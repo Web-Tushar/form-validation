dd6f2bb0a77d1d62b45eb5024407fe09c99c5aca
Web-Tushar <tusharimranvip895@gmail.com>
2024-01-20 00:58:07 +0600

    js add

---
+++ script.js
@@ -0,0 +1,15 @@
+let usename = document.querySelector(".usename")
+let email = document.querySelector(".email")
+let password = document.querySelector(".password")
+let cfnpssword = document.querySelector(".cfnpssword")
+let btn = domument.querySelector(".btn")
+let result_1 =document.querySelector("result_1")
+let result_2 =document.querySelector("result_2")
+let result_3 =document.querySelector("result_3")
+let result_4 =document.querySelector("result_4")
+
+btn.addEventListener("click",function(){
+     if(usename.value == ""){
+
+     }
+})
