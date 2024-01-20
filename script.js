ca9407618afb1f489d3f118305b0460a2a8d179a
Web-Tushar <tusharimranvip895@gmail.com>
2024-01-21 00:41:41 +0600

    problem solvd

--- script.js
+++ script.js
@@ -2,23 +2,52 @@ let usename = document.querySelector(".usename")
 let email = document.querySelector(".email")
 let password = document.querySelector(".password")
 let cfnpssword = document.querySelector(".cfnpssword")
-let btn = domument.querySelector(".btn")
-let result_1 = document.querySelector(".result_1")
-let result_2 = document.querySelector(".result_2")
-let result_3 = document.querySelector(".result_3")
-let result_4 = document.querySelector(".result_4")
+let btn = document.querySelector(".btn")
+let resultone = document.querySelector(".result_1")
+let resulttwo = document.querySelector(".result_2")
+let resultthree = document.querySelector(".result_3")
+let resultfour = document.querySelector(".result_4")
 
 let emailregex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 
 btn.addEventListener("click",function() {
+     // =======usename input====
+          if(usename.value == ""){
+               resultone.innerHTML = "pleas input your name"
+               usename.style.border = "1px dashed red"
+          } else{
+               resultone.innerHTML = ""
+               usename.style.border = ""
+          }
+
+
+
+
+     // =======email input====
      if(email.value == ""){
-          result_2.innerHTML = "pleas input your email"
+          resulttwo.innerHTML = "pleas input your email"
+          email.style.border = "1px dashed red"
 
      }else if(!email.value.match(emailregex)){
-          result_2.innerHTML = "email formate thik nai"
+          resulttwo.innerHTML = "email formate thik nai"
+          email.style.border = "1px dashed red"
      }
      else{
-          result_2.innerHTML = ""
+          resulttwo.innerHTML = ""
+          email.style.border = ""
      }
+     // =======password input====
+
+     if(password.value == ""){
+          resultthree.innerHTML = " pleas input your password"
+          password.style.border = "1px dashed red"
+     } else if(password.value.length > 20 || password.value.length < 12 ){
+          resultthree.innerHTML = " At least 12 characters long but 14 or more is better"
+          password.style.border = "1px dashed red"
+     } else{
+          resultthree.innerHTML = ""
+     }
+
+     
 })
